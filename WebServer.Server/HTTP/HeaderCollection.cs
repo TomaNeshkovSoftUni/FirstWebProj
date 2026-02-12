3ac778e99c4774a3e1a0a73bd74eaeb06e0f4016
TomaNeshkovSoftUni <toma.neshkov.highschool@buditel.bg>
2026-02-12 09:49:58 +0200

    Add project files.

---
+++ WebServer.Server/HTTP/HeaderCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServer.Server.HTTP_Request
+{
+    public class HeaderCollection : IEnumerable<Header>
+    {
+        private readonly Dictionary<string, Header> headers;
+        public HeaderCollection()
+        {
+            headers = new Dictionary<string, Header>();
+        }
+
+        public int Count => this.headers.Count;
+        public void Add(string name, string value)
+        {
+            var headers = new Header(name, value);
+            this.headers.Add(name, headers);
+        }
+
+        public IEnumerator<Header> GetEnumerator()
+        {
+            return headers.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ WebServer.Server/HTTP/Response.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace WebServer.Server.HTTP_Request
+{
+    public class Response
+    {
+        public StatusCode StatusCode { get; init; }
+        public HeaderCollection Headers { get;  } = new HeaderCollection();
+        public string? Body { get; set; }
+        public Response(StatusCode statusCode)
+        {
+            StatusCode = statusCode;
+
+            Headers.Add(Header.Server, "My Web Server");
+            Headers.Add(Header.Data, $"{DateTime.UtcNow:r}");
+        }
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            result.AppendLine($"HTTP/1.1 {(int)StatusCode} {StatusCode}");
+            foreach (var header in Headers)
+            {
+                result.AppendLine(header.ToString());
+            }
+            result.AppendLine();
+            if (!string.IsNullOrEmpty(Body))
+            {
+                result.AppendLine(Body);
+            }
+            return result.ToString();
+        }
+    }
+}
