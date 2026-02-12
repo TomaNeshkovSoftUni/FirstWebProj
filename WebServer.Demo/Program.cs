fd4f73b9a7459789d270e98970b9d719ae7dc982
TomaNeshkovSoftUni <toma.neshkov.highschool@buditel.bg>
2026-02-12 11:19:41 +0200

    Get requests

---
+++ WebServer.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using WebServer.Server;
+using WebServer.Server.Responses;
+
+namespace WebServer.demo
+{ 
+    public class Program
+    {
+        public static void Main()
+        {
+            var server = new HttpServer(x =>
+            x.MapGet("/", new TextResponse("<h1 style=\"color:blue;\">Hello from my html response!</h1>"))
+            .MapGet("/", new TextResponse("Hello from my server, now with routing table!!!")));
+
+            server.Start();
+        }
+    }
+}
