d09e5ccfe863d2924058f8973fe3803b3b65cf65
toman <toma.neshkov.highschool@buditel.bg>
2026-03-13 20:47:14 +0200

    Added changes for step 3

---
+++ WebServer.Server/HTTP/Session.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FirstWebServer.Server.Common;
+
+namespace FirstWebServer.Server.HTTP
+{
+    public class Session
+    {
+        public const string SessionCookieName = "MyWebServerSID";
+        public const string SessionCurrentDateKey = "CurrentDate";
+        public const string SessionUserKey = "AuthenticatedUserId";
+
+        private Dictionary<string, string> data;
+
+        public string Id { get; private set; }
+        public string this[string key]
+        {
+            get { return data[key]; }
+            set { data[key] = value; }
+        }
+
+        public Session(string id)
+        {
+            Guard.AgainstNull(id, nameof(id));
+
+            this.Id = id;
+            this.data = new Dictionary<string, string>();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return data.ContainsKey(key);
+        }
+        public void Clear()
+        {
+            data.Clear();
+        }
+    }
+}
